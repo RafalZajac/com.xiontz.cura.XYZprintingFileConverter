ddb6d917fb0613e90593c00063bfd37eb3d8238b
Rafal Zajac <rafal@xiontz.com>
2017-06-28 22:56:20 +0100

    Update DaVinciConvert.py

--- DaVinciConvert.py
+++ DaVinciConvert.py
@@ -6,7 +6,7 @@ class DaVinciConvert(Script):
     
     def getSettingDataString(self):
         return """{
-            "name": "XYZ DaVinci 1.1 converter",
+            "name": "da Vinci 1.1 Plus converter",
             "key": "DaVinciConvert",
             "metadata": {},
             "version": 2,
@@ -18,20 +18,12 @@ class DaVinciConvert(Script):
                     "description": "Ignore any nozzle or bed temperatures set in Cura and use the printer\'s default temperatures.",
                     "type": "bool",
                     "default_value": true
-                },
-                "replace_GO":
-                {
-                    "label": "Replace G0 with G1",
-                    "description": "When enabled, the G0 moves will be replaced with G1.",
-                    "type": "bool",
-                    "default_value": true
                 }
             }
         }"""
 
     def execute(self, data):
         ignore_temps = self.getSettingValueByKey("ignore_temp_settings")
-        replace_GO = self.getSettingValueByKey("replace_GO")
 
         for line in data[0].split('\n'):
             if ";Filament used: " in line:
@@ -47,16 +39,14 @@ class DaVinciConvert(Script):
             if ";LAYER_COUNT:" in line:
                 layer_count = line.replace(";LAYER_COUNT:","")
 
-        if replace_GO or ignore_temps:
-            for layer_number, layer in enumerate(data):
-                if replace_GO:
-                    data[layer_number] = layer.replace("G0 ","G1 ") #Replace G0
-                if ignore_temps:
-                    converted_lines = []
-                    for line in data[layer_number].split('\n'):
-                        if "M104" not in line and "M109" not in line and "M140" not in line and "M190" not in line:
-                            converted_lines += [line]
-                    data[layer_number] = '\n'.join(converted_lines)
+        for layer_number, layer in enumerate(data):
+            data[layer_number] = layer.replace("G0 ","G1 ") #Replace G0
+            if ignore_temps:
+                converted_lines = []
+                for line in data[layer_number].split('\n'):
+                    if "M104" not in line and "M109" not in line and "M140" not in line and "M190" not in line:
+                        converted_lines += [line]
+                data[layer_number] = '\n'.join(converted_lines)
 
         data[0] = self.getXYZHeader().format(print_time, filament, layer_height, layer_count)
         data[len(data)-1] += self.getXYZFooter()
